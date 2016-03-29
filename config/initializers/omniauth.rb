e3bd7ab55544f1b2513006eae622632563270bdf
Wakabayashi syunsuke <wkba61@gmail.com>
2016-03-29 23:27:55 +0900

    first commit

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter,
-    Rails.application.secrets.twitter_api_key,
-    Rails.application.secrets.twitter_api_secret
+  provider :twitter, "9JIaLOUpEf8Lik7GtsnPy3wFY", "oihXKmdDRR2EWuAyObwlmCPwY6AiC5FqKtpLvs57gdHJ00vRgO"
 end
